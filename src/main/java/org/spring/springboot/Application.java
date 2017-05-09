aa8f1d5ad5ca624b1ae85366a732a888672c8fac
kangwl <kangwl>
2017-05-09 10:19:16 +0800

    修改部分代码

--- src/main/java/org/spring/springboot/Application.java
+++ src/main/java/org/spring/springboot/Application.java
@@ -19,8 +19,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        // 程序启动入口
+        // 程序启动入口   git remote add origin https://github.com/wanlinmj/springboot-mybatis-thymeleaf.git
         // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
         SpringApplication.run(Application.class,args);
+
+        /**  20170509
+         * git 提交
+         *  1、 git remote add origin https://github.com/wanlinmj/springboot-mybatis-thymeleaf.git
+         *  2、 git push -u origin master
+         *  3、 git branch (若果报错： error: src refspec master does not match any. error: failed to push some refs to 'https://github.com/wanlinmj/springboot-mybatis-thymeleaf.git')
+         *  4、 git commit -m "test"
+         *  5、 git push -u origin master
+         */
+
     }
 }
